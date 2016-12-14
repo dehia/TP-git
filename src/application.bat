30db42c7feaef844a797b2e3d1017a57a287cb9b
Kahina Hellio <kahina.hellio@gmail.com>
2016-12-14 19:52:14 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
- ### application.bat - 2016 - Hellio ###
- echo "Copyright company - 2016"
- echo "Build : 10:00"
- echo "version 1.1"
+### application.bat - 201x - votre nom ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
