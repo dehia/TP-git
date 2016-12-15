7b577a9aaa26a5e5b1aff5ed9ce0189677568967
Michenski <boris.michenski@gmail.com>
2016-12-15 10:45:28 +0100

    c'est moi, MBV

--- src/application.bat
+++ src/application.bat
@@ -1,5 +1,6 @@
 ### application.bat - 2016 - Kahina Hellio ###
 ### application.bat - 2016 - Yoanne Randria ###
+### application.bat - 2016 - Boris Michenski ###
 version = "2"
 build_hour = "11H00"
 
